43f6eda0884f4bd5fd22a04eeb00325d72dad51b
K Harsh <harshnaidu2u@gmail.com>
2023-10-31 23:09:49 +0530

    final changes

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -6,5 +6,5 @@ import { ReactNode } from "react";
 export default function Provider ({children} : {children : ReactNode}) {
     return <ThemeProvider attribute="class">
          {children} 
-        </ThemeProvider>
+     </ThemeProvider>
 }
